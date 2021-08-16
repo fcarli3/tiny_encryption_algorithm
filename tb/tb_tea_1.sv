6aa4eed7125864d4dfc31cdefff21c9f6103b964
Francesco Carli <62562180+fcarli3@users.noreply.github.com>
2021-08-16 15:01:34 +0200

    Update tb_tea_1.sv

--- tb/tb_tea_1.sv
+++ tb/tb_tea_1.sv
@@ -18,8 +18,8 @@ module tb_tea_1;
   
   localparam NUL_CHAR = 8'h00; 
 
-  reg			    key_valid   = 1'b0;     // 1 = input data stable and valid, 0 = o.w.
-  reg			    ptxt_valid  = 1'b0;     // 1 = input data stable and valid, 0 = o.w.
+  reg	            key_valid   = 1'b0;     // 1 = input data stable and valid, 0 = o.w.
+  reg		    ptxt_valid  = 1'b0;     // 1 = input data stable and valid, 0 = o.w.
   reg      [63:0]   ptxt_blk    = NUL_CHAR; // plaintext
   reg      [127:0]  key         = NUL_CHAR; // key
   reg      [63:0]   expected_ct = NUL_CHAR; // expected ciphertext (from tv)
@@ -36,59 +36,59 @@ module tb_tea_1;
     ,.ptxt_valid                (ptxt_valid)
     ,.ptxt_blk                  (ptxt_blk)
     ,.key                       (key)
-	,.ctxt_blk                  (ctxt_blk)
-	,.ctxt_ready                (ctxt_ready)
+    ,.ctxt_blk                  (ctxt_blk)
+    ,.ctxt_ready                (ctxt_ready)
   );
     
 
-	initial begin
-	
-		@(posedge clk); 
-		// Set to ptxt_blk, key and expected_ct values that will be used to run the tests
-		ptxt_blk    = 64'h0000000000000000;
-		key         = 128'h80000000000000000000000000000000;		
-		expected_ct = 64'h9327C49731B08BBE;
-		
-		// PTXT_VALID = 1 - KEY_VALID = 1
-		@(posedge clk); 
-		ptxt_valid = 1'b1;
-		key_valid  = 1'b1;
-		
-		@(posedge clk); //at this moment the temporary register tmp_ctxt_ready is ready
-		@(posedge clk);	//at this moment the output register ctxt_ready is ready	
-		$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b1 ? "OK" : "ERROR");
-		
-		
-		// PTXT_VALID = 0 - KEY_VALID = 0
-		@(posedge clk);
-		ptxt_valid = 1'b0;
-		key_valid  = 1'b0;
-		
-		@(posedge clk); 
-		@(posedge clk);
-		$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b0 ? "OK" : "ERROR");
-		
-		
-		// PTXT_VALID = 1 - KEY_VALID = 0
-		@(posedge clk);
-		ptxt_valid = 1'b1;
-		key_valid  = 1'b0;
-		
-		@(posedge clk); 
-		@(posedge clk);
-		$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b0 ? "OK" : "ERROR");
-		
-		
-		// PTXT_VALID = 0 - KEY_VALID = 1
-		@(posedge clk);
-		ptxt_valid = 1'b0;
-		key_valid  = 1'b1;
-		
-		@(posedge clk); 
-		@(posedge clk);
-		$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b0 ? "OK" : "ERROR");
-		
-		$stop;
-	end
+initial begin
+
+	@(posedge clk); 
+	// Set to ptxt_blk, key and expected_ct values that will be used to run the tests
+	ptxt_blk    = 64'h0000000000000000;
+	key         = 128'h80000000000000000000000000000000;		
+	expected_ct = 64'h9327C49731B08BBE;
+
+	// PTXT_VALID = 1 - KEY_VALID = 1
+	@(posedge clk); 
+	ptxt_valid = 1'b1;
+	key_valid  = 1'b1;
+
+	@(posedge clk); //at this moment the temporary register tmp_ctxt_ready is ready
+	@(posedge clk);	//at this moment the output register ctxt_ready is ready	
+	$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b1 ? "OK" : "ERROR");
+
+
+	// PTXT_VALID = 0 - KEY_VALID = 0
+	@(posedge clk);
+	ptxt_valid = 1'b0;
+	key_valid  = 1'b0;
+
+	@(posedge clk); 
+	@(posedge clk);
+	$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b0 ? "OK" : "ERROR");
+
+
+	// PTXT_VALID = 1 - KEY_VALID = 0
+	@(posedge clk);
+	ptxt_valid = 1'b1;
+	key_valid  = 1'b0;
+
+	@(posedge clk); 
+	@(posedge clk);
+	$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b0 ? "OK" : "ERROR");
+
+
+	// PTXT_VALID = 0 - KEY_VALID = 1
+	@(posedge clk);
+	ptxt_valid = 1'b0;
+	key_valid  = 1'b1;
+
+	@(posedge clk); 
+	@(posedge clk);
+	$display("PTXT_VALID: %b - KEY_VALID: %b - CTXT_READY: %b - %-5s", ptxt_valid, key_valid, ctxt_ready, ctxt_ready === 1'b0 ? "OK" : "ERROR");
+
+	$stop;
+end
 
 endmodule
